e235e1650c12799087fa6f8382d4384ab23ba504
Sungjin Kim <jamessungjin.kim@gmail.com>
2019-10-30 00:34:57 +0900

    ex2_1 is updated to consider TF2

---
+++ tf2/ex1_1/tf2_ex1_1_simple_exercise.py
@@ -0,0 +1,14 @@
+from tensorflow import keras
+import numpy
+
+x = numpy.array([0, 1, 2, 3, 4])
+y = x * 2 + 1
+
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(1, input_shape=(1,)))
+model.compile('SGD', 'mse')
+
+model.fit(x[:2], y[:2], epochs=1000, verbose=0)
+
+print('Targets:', y[2:])
+print('Predictions:', model.predict(x[2:]).flatten())
---
+++ tf2/ex2_1_ann_mnist_cl.py
@@ -0,0 +1,141 @@
+##############################################
+# Modeling
+##############################################
+from tensorflow.keras import layers, models
+
+
+def ANN_models_func(Nin, Nh, Nout):
+    x = layers.Input(shape=(Nin,))
+    h = layers.Activation('relu')(layers.Dense(Nh)(x))
+    y = layers.Activation('softmax')(layers.Dense(Nout)(h))
+    model = models.Model(x, y)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def ANN_seq_func(Nin, Nh, Nout):
+    model = models.Sequential()
+    model.add(layers.Dense(Nh, activation='relu', input_shape=(Nin,)))
+    model.add(layers.Dense(Nout, activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+class ANN_models_class(models.Model):
+    def __init__(self, Nin, Nh, Nout):
+        # Prepare network layers and activate functions
+        hidden = layers.Dense(Nh)
+        output = layers.Dense(Nout)
+        relu = layers.Activation('relu')
+        softmax = layers.Activation('softmax')
+
+        # Connect network elements
+        x = layers.Input(shape=(Nin,))
+        h = relu(hidden(x))
+        y = softmax(output(h))
+
+        super().__init__(x, y)
+        self.compile(loss='categorical_crossentropy',
+                     optimizer='adam', metrics=['accuracy'])
+
+
+class ANN_seq_class(models.Sequential):
+    def __init__(self, Nin, Nh, Nout):
+        super().__init__()
+        self.add(layers.Dense(Nh, activation='relu', input_shape=(Nin,)))
+        self.add(layers.Dense(Nout, activation='softmax'))
+        self.compile(loss='categorical_crossentropy',
+                     optimizer='adam', metrics=['accuracy'])
+
+
+##############################################
+# Data
+##############################################
+import numpy as np
+from tensorflow.keras import datasets  # mnist
+from tensorflow.keras import utils  # to_categorical
+
+
+def Data_func():
+    (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
+
+    Y_train = utils.to_categorical(y_train)
+    Y_test = utils.to_categorical(y_test)
+
+    L, W, H = X_train.shape
+    X_train = X_train.reshape(-1, W * H)
+    X_test = X_test.reshape(-1, W * H)
+
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
+
+    return (X_train, Y_train), (X_test, Y_test)
+
+
+##############################################
+# Plotting
+##############################################
+import matplotlib.pyplot as plt
+
+
+def plot_acc(history, title=None):
+    # summarize history for accuracy
+    if not isinstance(history, dict):
+        history = history.history
+
+    plt.plot(history['acc'])
+    plt.plot(history['val_acc'])
+    if title is not None:
+        plt.title(title)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Training', 'Verification'], loc=0)
+    # plt.show()
+
+
+def plot_loss(history, title=None):
+    # summarize history for loss
+    if not isinstance(history, dict):
+        history = history.history
+
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    if title is not None:
+        plt.title(title)
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Training', 'Verification'], loc=0)
+    # plt.show()
+
+
+##############################################
+# Main
+##############################################
+def main():
+    Nin = 784
+    Nh = 100
+    number_of_class = 10
+    Nout = number_of_class
+
+    # model = ANN_models_func(Nin, Nh, Nout)
+    # model = ANN_models_class(Nin, Nh, Nout)
+    model = ANN_seq_class(Nin, Nh, Nout)
+    (X_train, Y_train), (X_test, Y_test) = Data_func()
+
+    ##############################################
+    # Training
+    ##############################################
+    history = model.fit(X_train, Y_train, epochs=15, batch_size=100, validation_split=0.2)
+    performace_test = model.evaluate(X_test, Y_test, batch_size=100, verbose=0)
+    print('Test Loss and Accuracy ->', performace_test)
+
+    plot_loss(history)
+    plt.show()
+    plot_acc(history)
+    plt.show()
+
+
+# Run code
+if __name__ == '__main__':
+    main()
